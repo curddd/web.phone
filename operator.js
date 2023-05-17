b3b6ba9a06cf9241c8471abee061d4d7b1987958
Your Name <you@example.com>
2023-05-17 14:25:28 +0200

    first commit connection stands

---
+++ operator.js
@@ -0,0 +1,76 @@
+const WebSocket = require('ws')
+
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+
+var number_to_clients = new Map();
+
+var calling_list = [];
+
+wss.on('connection', (ws) => {
+	console.log('Client connected');
+	let my_number = getRandomNumber();
+
+	ws.send(JSON.stringify({type: 'number_assign', number: my_number}));
+	
+	number_to_clients.set(my_number,ws);
+
+
+	ws.on('message', (message) => {
+		console.log(`Received message: ${message}`);
+		let parsed = JSON.parse(message);
+
+		if(parsed.type == 'iceCandidate'){
+			number_to_clients.get(parsed.number).send(JSON.stringify({type: 'iceCandidate', caller: my_number, data: parsed.data}));
+		}
+
+
+		if(parsed.type == 'CALL_OFFER'){
+			let number = parsed.number;
+			if(!number){
+				return;
+			}
+			if(!number_to_clients.has(number)){
+				ws.send(JSON.stringify({type: 'error', msg:"Sorry, that number is not known to me"}));
+				return;
+			}
+
+			//number_to_clients.get(number).send(`INCOMING_CALL ${my_number}`);
+			calling_list.push(`${my_number} to ${number}`);
+			number_to_clients.get(number).send(JSON.stringify(parsed));
+		}
+
+		if(parsed.type=='CALL_ANSWER'){
+			let number = parsed.number;
+			if(!number || !(calling_list.includes(`${number} to ${my_number}`))){
+				return;
+			}
+
+			number_to_clients.get(number).send(JSON.stringify(parsed));
+
+		}
+		
+		if(parsed.type=='DENY'){
+			let number = parsed.number;
+			if(!number || calling_list.includes(`${number} to ${my_number}`)){
+				return;
+			}
+			number_to_clients.get(number).send("The one you called hung up");
+			
+			calling_list.splice(calling_list.indexOf(`${number} to ${my_number}`),1);
+		}
+
+	});
+
+	ws.on('close', () => {
+		console.log('Client disconnected');
+		number_to_clients.delete(my_number);
+	});
+});
+
+function getRandomNumber() {
+	const prefix = Math.floor(Math.random() * 9000) + 1000;
+	const suffix = Math.floor(Math.random() * 900000) + 100000;
+	return `${prefix}-${suffix}`;
+}
---
+++ webaudio.js
@@ -0,0 +1,134 @@
+var localStream;
+var peerConnection;
+
+let channel = null
+
+function createPeerConnection(number) {
+    // Create a new RTCPeerConnection
+    peerConnection = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] });
+
+    // Event handler for ICE candidates
+    peerConnection.addEventListener('icecandidate', (event) => {
+        if (event.candidate) {
+            // Send ICE candidate to the other client
+            socket.send(JSON.stringify({
+                type: 'iceCandidate',
+                number: number,
+                data: event.candidate
+            }));
+        }
+    });
+
+    // Event handler for receiving remote audio tracks
+    peerConnection.addEventListener('track', (event) => {
+        if (event.track.kind === 'audio') {
+            // Add the remote audio track to the audio element
+            remoteAudio.srcObject = event.streams[0];
+        }
+    });
+    return peerConnection;
+
+}
+
+
+var localStream;
+
+// Get user media for audio
+navigator.mediaDevices.getUserMedia({ audio: true })
+.then((stream) => {
+  localStream = stream;
+})
+.catch((error) => {
+  console.log('Error accessing user media:', error);
+});
+
+
+
+function startCallAndCreateOffer() {
+    let number = document.getElementById('number').value;
+
+    // Create a new RTCPeerConnection
+    createPeerConnection(number);
+
+    // Add the local audio stream to the peer connection
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    });
+
+    // Create and send offer
+    peerConnection.createOffer()
+        .then((offer) => {
+            return peerConnection.setLocalDescription(offer);
+        })
+        .then(() => {
+            // Send offer to the other client
+            socket.send(JSON.stringify({
+                type: 'CALL_OFFER',
+                caller: my_number,
+                number: number,
+                data: peerConnection.localDescription
+            }));
+        })
+        .catch((error) => {
+            console.log('Error creating offer:', error);
+        });
+}
+
+function startCallAndHandleOffer(parsed) {
+   
+    // Create a new RTCPeerConnection
+    createPeerConnection(parsed.caller);
+
+    // Add the local audio stream to the peer connection
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    });
+    handleOffer(parsed);
+}
+
+
+function handleOffer(parsed) {
+    // Set remote description and create answer
+    peerConnection.setRemoteDescription(new RTCSessionDescription(parsed.data))
+        .then(() => {
+            return peerConnection.createAnswer();
+        })
+        .then((answer) => {
+            return peerConnection.setLocalDescription(answer);
+        })
+        .then(() => {
+            // Send the answer back to the caller
+            socket.send(JSON.stringify({
+                type: 'CALL_ANSWER',
+                caller: my_number,
+                number: parsed.caller,
+                data: peerConnection.localDescription
+            }));
+        })
+        .catch((error) => {
+            console.log('Error handling offer:', error);
+        });
+}
+
+
+function handleAnswer(number, answer) {
+    // Set the remote description with the answer
+    peerConnection.setRemoteDescription(new RTCSessionDescription(answer))
+        .then(() => {
+            console.log('Answer from', number, 'is set as remote description.');
+        })
+        .catch((error) => {
+            console.log('Error handling answer:', error);
+        });
+}
+
+function handleIceCandidate(candidate) {
+    // Add the received ICE candidate to the peer connection
+    peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
+      .then(() => {
+        console.log('ICE candidate added successfully.');
+      })
+      .catch((error) => {
+        console.log('Error adding ICE candidate:', error);
+      });
+}
